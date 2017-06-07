ff3997d36d24f0497e6e8c615ae115ad3434d891
aperry39 <aperry94@gatech.edu>
2017-06-07 17:25:38 -0400

    Space bar works for clicking

--- customjs.js
+++ customjs.js
@@ -164,10 +164,15 @@ var trackSector = function(sector) {
         }
         // mouseDown--;
     }
-    if (calibrationComplete == 1 && sectorCounters[sector - 1] > 100) {
+    if (calibrationComplete == 1) {
         //console.log("HERE");
+		document.body.onkeyup = function(e){
+	    if(e.keyCode == 32){
+			console.log("THE FUCK DUDE?!?");
+	    }
         addToArray(sector);
     }
+}
 }
 
 var classifer = function(x, y){
